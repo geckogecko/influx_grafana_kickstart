998ebc69f74fdb02eb50b2b3d97cb43eca150162
Georg Steinbacher <georg.steinbacher@ncr.com>
2018-11-21 07:23:49 +0100

    impros

--- getOpenWeatherDataToInflux.py
+++ getOpenWeatherDataToInflux.py
@@ -8,156 +8,34 @@ def getWeatherDetailsAtCords(owm, lat, lng):
     observation = owm.weather_at_coords(lat, lng)
     return observation.get_weather()
 
-
-def sendWindData(windSpeed, windDeg, client):
-    json_body = [
-        {
+def sendDictToInflux(dict, client):
+    if dict:
+        json_body = {
             "measurement": "open_weather_data",
             "tags": {
                 "location": "home",
-                "type": "wind"
             },
-            "fields": {
-                "speed": windSpeed,
-                "deg": windDeg
-            }
+            "fields": {}
         }
-    ]
 
-    client.write_points(json_body)
+        for key, value in dict.iteritems():
+            json_body['fields'][key] = value
 
-def sendHumidityData(humidity, client):
-    json_body = [
-        {
+        client.write_points(json_body)
+
+def sendToInflux(dataName, value, client):
+     if value is not None:
+        json_body = {
             "measurement": "open_weather_data",
             "tags": {
                 "location": "home",
-                "type": "humidity"
             },
             "fields": {
-                "humidity": humidity
+                dataName: value
             }
         }
-    ]
 
-    client.write_points(json_body)
-
-def sendTemperatureData(temp, temp_max, temp_min, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "temperature"
-            },
-            "fields": {
-                "temp": temp,
-                "temp_max": temp_max,
-                "temp_min": temp_min
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
-def sendRainData(rain_1h, rain_3h, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "rain"
-            },
-            "fields": {
-                "rain_1h": rain_1h,
-                "rain_3h": rain_3h
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
-def sendSnowData(snow_1h, snow_3h, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "snow"
-            },
-            "fields": {
-                "snow_1h": snow_1h,
-                "snow_3h": snow_3h
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
-def sendCloudData(coverage, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "cloud_coverage"
-            },
-            "fields": {
-                "coverage": coverage
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
-def sendHeatIndexData(heat_index, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "heat_index"
-            },
-            "fields": {
-                "heat_index": heat_index
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
-def sendPreasureData(preasure, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "preasure"
-            },
-            "fields": {
-                "preasure": preasure
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
-def sendVisibleDistanceData(visibleDistance, client):
-    json_body = [
-        {
-            "measurement": "open_weather_data",
-            "tags": {
-                "location": "home",
-                "type": "visible_distance"
-            },
-            "fields": {
-                "visibleDistance": visibleDistance
-            }
-        }
-    ]
-
-    client.write_points(json_body)
-
+        client.write_points(json_body)
 
 def main(argv):
     owm = pyowm.OWM('76b37d4118ddc94a41b2344a4b2dc84a') #openWeatherApiKey
@@ -166,22 +44,17 @@ def main(argv):
     while True:
         w = getWeatherDetailsAtCords(owm, 48.016686, 13.487734)
 
-        if 'speed' in w.get_wind() and 'deg' in w.get_wind():
-            sendWindData(w.get_wind()['speed'], w.get_wind()['deg'], client)
+        sendDictToInflux(w.get_wind(), client)
+        sendDictToInflux(w.get_temperature('celsius'), client)
+        sendDictToInflux(w.get_rain(), client)
+        sendDictToInflux(w.get_snow(), client)
+        sendDictToInflux(w.get_pressure(), client)
         
-        sendHumidityData(w.get_humidity(), client)
-        sendTemperatureData(w.get_temperature('celsius')['temp'], w.get_temperature('celsius')['temp_max'], w.get_temperature('celsius')['temp_min'], client)
-        
-        #sendRainData(w.get_rain()['1h'], w.get_rain()['3h'], client)
-        #sendSnowData(w.get_snow()['1h'], w.get_snow()['3h'], client)
-        
-        sendCloudData(w.get_clouds(),client)
-
-        if w.get_heat_index() is not None:
-            sendHeatIndexData(w.get_heat_index(),client)
-            
-        sendPreasureData(w.get_pressure()['press'], client)
-        sendVisibleDistanceData(w.get_visibility_distance(), client)
+        sendToInflux('hum', w.get_humidity(), client)
+        sendToInflux('clouds', w.get_clouds(), client)
+        sendToInflux('heat_index', w.get_heat_index(), client)
+        sendToInflux('visible_distance', w.get_visibility_distance(), client)
+    
         time.sleep(60)
 
 
